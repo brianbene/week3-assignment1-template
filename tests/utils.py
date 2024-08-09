3ff894c89efdf6e0225440adeca78c204c0dac9b
brianbene <bmb91@duke.edu>
2024-08-08 21:51:56 -0400

    Update utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -1,17 +1,18 @@
-
 def str_to_bool(val):
     """
     Convert a string representation of truth to True or False
-    True values are 'y', 'yes', or ''; case-insensitive
-    False values are 'n', or 'no'; case-insensitive
+    True values are 'y', 'yes', '1', or ''; case-insensitive
+    False values are 'n', 'no', or '0'; case-insensitive
     Raises ValueError if 'val' is anything else.
     """
-    true_vals = ['yes', 'y', '']
-    false_vals = ['no', 'n']
+    true_vals = ['yes', 'y', '1', '']
+    false_vals = ['no', 'n', '0']
+    
     try:
         val = val.lower()
     except AttributeError:
         val = str(val).lower()
+        
     if val in true_vals:
         return True
     elif val in false_vals:
